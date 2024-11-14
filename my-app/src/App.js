962d00ec51c47d2c0b58eaa5e612439e48712267
Sheystone <139350899+Sheystone@users.noreply.github.com>
2024-11-14 16:51:00 +0200

    new styles

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,31 @@
 
 import './App.css';
 
+import { IoMdAddCircle } from "react-icons/io";
+
+import { ImBin } from "react-icons/im";
+
+import { FaUndoAlt } from "react-icons/fa";
+
 function App() {
   return (
     <div className="App">
+      <header>
+       
+      <IoMdAddCircle className='add'/>
+     
+      <ImBin className='add'/>
+      
+      <FaUndoAlt className='add' />
+      </header>
+      <main>
     <h1>To do list:</h1>
+    </main>
+    <footer>
+      <h2>Plan your day with TO DO LIST!</h2>
+    </footer>
     </div>
+   
   );
 }
 
