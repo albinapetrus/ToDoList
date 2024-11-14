d7c2dc8ae546805a3fd3ad7d2565e9ed22a657a9
Sheystone <139350899+Sheystone@users.noreply.github.com>
2024-11-14 18:01:13 +0200

    new functions

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,6 +6,12 @@ import { IoMdAddCircle } from "react-icons/io";
 import { ImBin } from "react-icons/im";
 
 import { FaUndoAlt } from "react-icons/fa";
+const showNothing = () => {
+  return (<div className='empty'>
+    <h2>Планів нема.....       Додайте пункти до списку справ</h2>
+  </div>)
+}
+
 
 function App() {
   return (
@@ -20,12 +26,13 @@ function App() {
       </header>
       <main>
     <h1>To do list:</h1>
+    {showNothing()}
     </main>
     <footer>
       <h2>Plan your day with TO DO LIST!</h2>
     </footer>
     </div>
-   
+  
   );
 }
 
