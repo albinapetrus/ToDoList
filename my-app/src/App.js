5ae59a7cc02d56d3836f8068f669c2aaa701b78e
Sheystone <139350899+Sheystone@users.noreply.github.com>
2024-11-16 18:01:41 +0200

    new functions

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,11 +6,23 @@ import { IoMdAddCircle } from "react-icons/io";
 import { ImBin } from "react-icons/im";
 
 import { FaUndoAlt } from "react-icons/fa";
+
+
+function isEmpty(){
+  if(list.length<=0){
+   return showNothing();
+  }
+  return null;
+}
 const showNothing = () => {
   return (<div className='empty'>
     <h2>Планів нема.....       Додайте пункти до списку справ</h2>
   </div>)
 }
+const [list, setList] = useState([]); // Стан для масиву списку
+  const [inputValue, setInputValue] = useState(''); // Стан для введеного тексту
+  const [showInput, setShowInput] = useState(false); // Стан для контролю видимості інпуту
+
 
 
 function App() {
@@ -26,7 +38,7 @@ function App() {
       </header>
       <main>
     <h1>To do list:</h1>
-    {showNothing()}
+    {isEmpty()}
     </main>
     <footer>
       <h2>Plan your day with TO DO LIST!</h2>
